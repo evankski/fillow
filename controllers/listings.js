70a0e14ff4fac2175fb5d49bbed5a3f3860b4e05
evankski <evankarwowski@gmail.com>
2022-03-05 11:16:53 -0800

    adds some css

--- controllers/listings.js
+++ controllers/listings.js
@@ -31,11 +31,11 @@ router.post("/", (req, res) => {
 
 // GET /listings/new - display form for creating new listing
 router.get("/new", (req, res) => {
-  const user = res.locals.user
+  const user = res.locals.user;
   db.user
     .findAll()
     .then((users) => {
-      res.render("listings/new", { users: users, user:user });
+      res.render("listings/new", { users: users, user: user });
     })
     .catch((error) => {
       res.status(400).render("main/404");
@@ -44,10 +44,10 @@ router.get("/new", (req, res) => {
 
 // GET /listings/:id - display a specific listing
 router.get("/:id", async (req, res) => {
-  const user = res.locals.user
+  const user = res.locals.user;
   const comment = await db.comment.findAll({
-    where: {listingId: req.params.id}
-  })
+    where: { listingId: req.params.id },
+  });
   db.listing
     .findOne({
       where: { id: req.params.id },
@@ -56,40 +56,40 @@ router.get("/:id", async (req, res) => {
     .then((listing) => {
       if (!listing) throw Error();
       geocodingClient
-    .forwardGeocode({
-      query: `${listing.address} ${listing.city}, ${listing.state}`,
-      // autocomplete: false,
-      // limit: 1
-    })
-    .send()
-    .then((response) => {
-      if (
-        !response ||
-        !response.body ||
-        !response.body.features ||
-        !response.body.features.length
-      ) {
-        console.error("Invalid response:");
-        console.error(response);
-        return;
-      }
-      const feature = response.body.features[0];
-      // console.log(feature)
-      comment.forEach( async (element,i) => {
-        comment[i].user = await db.user.findOne({
-          where: {id: element.userId}
+        .forwardGeocode({
+          query: `${listing.address} ${listing.city}, ${listing.state}`,
+          // autocomplete: false,
+          // limit: 1
         })
-      })
-      res.render("listings/show", {
-        listing: listing,
-        mapkey: process.env.MAPBOX_TOKEN,
-        match: feature,
-        user: user,
-        comment: comment
-      });
-    });
-    //   console.log(listing.user);
-    //   res.render("listings/show", { listings: listing });
+        .send()
+        .then((response) => {
+          if (
+            !response ||
+            !response.body ||
+            !response.body.features ||
+            !response.body.features.length
+          ) {
+            console.error("Invalid response:");
+            console.error(response);
+            return;
+          }
+          const feature = response.body.features[0];
+          // console.log(feature)
+          comment.forEach(async (element, i) => {
+            comment[i].user = await db.user.findOne({
+              where: { id: element.userId },
+            });
+          });
+          res.render("listings/show", {
+            listing: listing,
+            mapkey: process.env.MAPBOX_TOKEN,
+            match: feature,
+            user: user,
+            comment: comment,
+          });
+        });
+      //   console.log(listing.user);
+      //   res.render("listings/show", { listings: listing });
     })
     .catch((error) => {
       console.log(error);
@@ -97,13 +97,13 @@ router.get("/:id", async (req, res) => {
     });
 });
 
-router.put('/edit/:id', async (req,res) => {
+router.put("/edit/:id", async (req, res) => {
   try {
     const foundListing = await db.listing.findOne({
-        where: {
-            // userId: res.locals.user.id,
-            id: req.params.id,
-        },
+      where: {
+        // userId: res.locals.user.id,
+        id: req.params.id,
+      },
     });
     foundListing.update({
       userId: req.body.userId,
@@ -114,29 +114,28 @@ router.put('/edit/:id', async (req,res) => {
       url: req.body.url,
     });
     await foundListing.save();
-    res.redirect('/');
-} catch (err) {
+    res.redirect("/");
+  } catch (err) {
     console.log("err", err);
-}
-})
-router.get('/edit/:id', async (req,res) => {
-      res.render('listings/edit.ejs', {listingId: req.params.id})
-})
-
-router.delete("/:id", async (req, res) => {
-
-      try {
-          const foundListing = await db.listing.findOne({
-              where: { id: req.params.id },
-          });
-          await foundListing.destroy();
-          res.redirect("/");
-      } catch (err) {
-          console.log(err);
-      }
+  }
+});
+router.get("/edit/:id", async (req, res) => {
+  res.render("listings/edit.ejs", { listingId: req.params.id});
 });
 
-router.post('/comments', (req,res) => {
+router.delete("/:id", async (req, res) => {
+  try {
+    const foundListing = await db.listing.findOne({
+      where: { id: req.params.id },
+    });
+    await foundListing.destroy();
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/comments", (req, res) => {
   db.comment
     .create({
       listingId: req.body.listingId,
@@ -150,7 +149,7 @@ router.post('/comments', (req,res) => {
     .catch((error) => {
       res.status(400).render("main/404");
     });
-})
+});
 
 // GET /faves -- READ all faves from the database
 // router.get('/', async (req, res) => {
@@ -161,7 +160,5 @@ router.post('/comments', (req,res) => {
 //     console.log(err)
 //   }
 // })
-
-
 
 module.exports = router;
